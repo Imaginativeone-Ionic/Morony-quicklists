82311740c3c558cb40ba5c56b08581a0f5171895
Doug Franklin <doug@dougfranklin.com>
2017-03-10 13:15:20 -0500

    App Module Upgraded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { Storage }       from '@ionic/storage';               // page 141
+import { MyApp }         from './app.component';
+import { HomePage }      from '../pages/home/home';
+import { IntroPage }     from '../pages/intro/intro';         // page 141
+import { ChecklistPage } from '../pages/checklist/checklist'; // page 141
+import { Data }          from '../providers/data';            // page 141
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    IntroPage,
+    ChecklistPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -14,8 +20,10 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    IntroPage,
+    ChecklistPage
   ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}, Storage, Data]
 })
 export class AppModule {}
