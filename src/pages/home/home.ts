015e21a55e1b3749ef2647663a15efc55c01dd88
Doug Franklin <doug@dougfranklin.com>
2017-03-14 09:59:18 -0400

    addChecklist Updated

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
-
-import { NavController } from 'ionic-angular';
+import { Component }      from '@angular/core';
+import { NavController, AlertController, Platform } from 'ionic-angular';
+import { ChecklistPage }  from '..checklist/checklist';
+import { ChecklistModel } from '../../models/checklist-model';
+import { Data }           from '../../providers/data';
+import { Keyboard }       from 'ionic-native';
 
 @Component({
   selector: 'page-home',
@@ -8,8 +11,62 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  checklists: ChecklistModel[] = [];
+
+  constructor(public nav: NavController, public dataService: Data, public alertCtrl: AlertController,
+      public platform: Platform) {
     
   }
+  
+  ionViewDidLoad() {}
+
+  addCheckList(): void {
+
+    let prompt = this.alertCtrl.create({
+
+      title: 'New Checklist',
+      message: 'Enter the name of your new checklist below:',
+      inputs: [{
+
+        name: 'name'
+
+      }],
+      buttons: [
+        {
+          text: 'Cancel'
+        },
+        {
+          text: 'Save',
+          handler: data => {
+
+            let newChecklist = new ChecklistModel(data.name, []);
+
+            this.checklists.push(newChecklist);
+
+            newChecklist.checkListUpdates().subscribe(update => {
+
+              this.save();
+
+            });
+
+            this.save();
+
+          }
+        }
+      ],
+
+    });
+
+    prompt.present();
+
+  }
+
+  renameChecklist(checklist): void {}
+
+  viewChecklist(checklist): void {}
+
+  removeChecklist(checklist): void {}
+
+  save(): void {}
 
 }
